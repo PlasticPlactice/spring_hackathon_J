5f27c85480944c6d2f7204793a038c93af70f0a9
yutohatano <y.hatano.sys23@morijyobi.ac.jp>
2025-04-04 10:16:07 +0900

    マイグレーションファイルの作成と書き込み

---
+++ database/migrations/2025_04_03_031930_create_y_subjects_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('y_subjects', function (Blueprint $table) {
+            $table->string('course_list_id')->primary();
+
+            $table->foreign('course_list_id')->references('id')->on('course_lists');
+
+            $table->int('subject_id');
+            $table->foreign('subject_id')->references('id')->on('subjects');
+
+            $table->string('detail');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('y_subjects');
+    }
+};
---
+++ database/migrations/2025_04_03_032015_create_t_comments_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('t_comments', function (Blueprint $table) {
+            $table->id();
+
+            $table->int('y_subject_id');
+            $table->int('teacher_id');
+            $table->foreign('y_subject_id')->references('id')->on('y_subjects');
+            $table->foreign('teacher_id')->references('id')->on('teachers');
+
+            $table->string('title');
+            $table->string('detail');
+            $table->boolean('link_flg')->default(false);
+            $table->boolean('del_flg')->default(false);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('t_comments');
+    }
+};
