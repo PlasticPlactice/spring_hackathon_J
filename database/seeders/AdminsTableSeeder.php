756e112f7d85d6d81c82fbeda05c1525ffe0a79e
Nayuta <n.kumagai.sys23@morijyobi.ac.jp>
2025-04-07 13:27:15 +0900

    シーダーの変更

--- database/seeders/AdminsTableSeeder.php
+++ database/seeders/AdminsTableSeeder.php
@@ -5,6 +5,8 @@ namespace Database\Seeders;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+use Carbon\Carbon;
 
 class AdminsTableSeeder extends Seeder
 {
@@ -15,10 +17,11 @@ class AdminsTableSeeder extends Seeder
     {
         $param = [
             'id' => 'admin@example.com',
-            'pw' => 'admin',
-            'del_flg' => '0'
+            'pw' => Hash::make('admin'),  // パスワードをハッシュ化
+            'del_flg' => '0',
+            'created_at' => Carbon::now(),  // 現在の日時を設定
+            'updated_at' => Carbon::now(),  // 現在の日時を設定
         ];
         DB::table('admins')->insert($param);
-
     }
 }
--- database/seeders/StudentsTableSeeder.php
+++ database/seeders/StudentsTableSeeder.php
@@ -5,6 +5,8 @@ namespace Database\Seeders;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+use Carbon\Carbon; 
 
 class StudentsTableSeeder extends Seeder
 {
@@ -15,32 +17,39 @@ class StudentsTableSeeder extends Seeder
     {
         $param = [
             'id' => 'student1@example.com',
-            'pw' => 'student1',
+            'pw' => Hash::make('student1'),
             'name' => '田中太郎',
             'del_flg' => '0',
             'img_path' => '',
             'entrance_year' => '2023',
-            'department_id' => '1'
+            'department_id' => '1',
+            'created_at' => Carbon::now(),  // 現在の日時を設定
+            'updated_at' => Carbon::now(),  // 現在の日時を設定
+            
         ];
         DB::table('students')->insert($param);
         $param = [
             'id' => 'student2@example.com',
-            'pw' => 'student2',
+            'pw' => Hash::make('student3'),
             'name' => '今野拓郎',
             'del_flg' => '0',
             'img_path' => '',
             'entrance_year' => '2024',
-            'department_id' => '2'
+            'department_id' => '2',
+            'created_at' => Carbon::now(),  // 現在の日時を設定
+            'updated_at' => Carbon::now(),  // 現在の日時を設定
         ];
         DB::table('students')->insert($param);
         $param = [
             'id' => 'student3@example.com',
-            'pw' => 'student3',
+            'pw' => Hash::make('student3'),
             'name' => '高橋花子',
             'del_flg' => '0',
             'img_path' => '',
             'entrance_year' => '2025',
-            'department_id' => '3'
+            'department_id' => '3',
+            'created_at' => Carbon::now(),  // 現在の日時を設定
+            'updated_at' => Carbon::now(),  // 現在の日時を設定
         ];
         DB::table('students')->insert($param);
     }
