e292bf5c5e992856d79f436b64a5e04d55956073
shibaya <m.shibaya.sys24@morijyobi.ac.jp>
2025-04-11 12:04:24 +0900

    テストページ作成

---
+++ public/js/subject_page.js
@@ -0,0 +1,11 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const tabs = document.querySelectorAll("#page-tab h2");
+  
+    tabs.forEach(tab => {
+      tab.addEventListener("click", function () {
+        tabs.forEach(t => t.classList.remove("active"));
+        this.classList.add("active");
+      });
+    });
+  });
+  
