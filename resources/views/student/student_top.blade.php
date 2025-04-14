94493781d0d2449fa813bd292d5e9b4c83676e11
kkudou <k.kudou.sys23@morijyobi.ac.jp>
2025-04-14 12:33:28 +0900

    生徒トップページを作成

--- resources/views/student/student_top.blade.php
+++ resources/views/student/student_top.blade.php
@@ -1,12 +1,125 @@
-<!DOCTYPE html>
-<html lang="ja">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    生徒トップ
-    <p>{{dd($cSubject)}}</p>
-</body>
-</html>
+@extends('layouts.base')
+@section('title','生徒トップ')
+@section('external_css')
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200&icon_names=star" />
+@endsection
+
+@section('side_bar_content')
+    <li class="side-bar-item" id="content-1" tabindex="0">
+        <a href="#">科目一覧</a>
+    </li>
+    @component('components.side_bar_menu')
+        @slot('content_id')
+            2
+        @endslot
+        @slot('side_bar_menu')
+            時間割
+        @endslot
+        @slot('register_link')
+            https://www.google.co.jp/
+        @endslot
+        @slot('edit_link')
+            https://www.yahoo.co.jp/
+        @endslot
+    @endcomponent
+@endsection
+
+@section('content')
+<div id="student-top-container">
+    <div>
+        <table class="table subject-table">
+            <tr>
+                <td></td>
+                <th class="th-horizontal">月</th>
+                <th class="th-horizontal">火</th>
+                <th class="th-horizontal">水</th>
+                <th class="th-horizontal">木</th>
+                <th class="th-horizontal">金</th>
+            </tr>
+
+            <tr>
+                <th class="th-vertical">1限</th>
+                <td class="subject-td">ダミー科目</td>
+                <td class="subject-td">ダミー科目</td>
+                <td class="subject-td">ダミー科目</td>
+                <td class="subject-td">ダミー科目</td>
+                <td class="subject-td">ダミー科目</td>
+            </tr>
+            
+            <tr>
+                <th class="th-vertical">2限</th>
+                <td class="subject-td">ダミー科目</td>
+                <td class="subject-td">ダミー科目</td>
+                <td class="subject-td">ダミー科目</td>
+                <td class="subject-td">ダミー科目</td>
+                <td class="subject-td">ダミー科目</td>
+            </tr>
+            
+            <tr>
+                <th class="th-vertical">3限</th>
+                <td class="subject-td">ダミー科目</td>
+                <td class="subject-td">ダミー科目</td>
+                <td class="subject-td">ダミー科目</td>
+                <td class="subject-td">ダミー科目</td>
+                <td class="subject-td">ダミー科目</td>
+            </tr>
+            
+            <tr>
+                <th class="th-vertical">4限</th>
+                <td class="subject-td">ダミー科目</td>
+                <td class="subject-td">ダミー科目</td>
+                <td class="subject-td">ダミー科目</td>
+                <td class="subject-td">ダミー科目</td>
+                <td class="subject-td">ダミー科目</td>
+            </tr>
+        </table>
+
+        <div class="favorite-page-section">
+            <h2>お気に入りページ</h2>
+            <table class="table favorite-page-table">
+                <tr>
+                    <th class="th-horizontal"></th>
+                    <th class="th-horizontal">ページ名</th>
+                    <th class="th-horizontal">カテゴリ</th>
+                </tr>
+                
+                <tr>
+                    <td class="favorite-td-star">
+                        <a href="#">
+                            <span class="material-symbols-outlined favorite-star-icon">star</span>
+                        </a>
+                    </td>
+                    <td class="favorite-td-name">
+                        <a href="#">サブページ名</a>
+                    </td>
+                    <td class="favorite-td-category">サブページカテゴリ</td>
+                </tr>
+                
+                <tr>
+                    <td class="favorite-td-star">
+                        <a href="#">
+                            <span class="material-symbols-outlined favorite-star-icon">star</span>
+                        </a>
+                    </td>
+                    <td class="favorite-td-name">
+                        <a href="#">サブページ名</a>
+                    </td>
+                    <td class="favorite-td-category">サブページカテゴリ</td>
+                </tr>
+                <tr>
+                    <td class="favorite-td-star">
+                        <a href="#">
+                            <span class="material-symbols-outlined favorite-star-icon">star</span>
+                        </a>
+                    </td>
+                    <td class="favorite-td-name">
+                        <a href="#">サブページ名</a>
+                    </td>
+                    <td class="favorite-td-category">サブページカテゴリ</td>
+                </tr>
+            </table>
+        </div>
+
+    </div>
+</div>
+@endsection
